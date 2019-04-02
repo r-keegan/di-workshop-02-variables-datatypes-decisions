da8915dd8e748570b4e2c79bfde3927378212a0a
Rebecca Keegan <rebeccachristinekeegan@gmail.com>
2019-04-02 20:40:27 +0100

    Added a count + fixed up bounce on rt handside

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -1,25 +1,27 @@
+const squareSize = 10
 let position = 100;
 let moving = true;
 let amountToMovePerCycle = 3;
+let count = 0;
 
 function setup() {
-  createCanvas(600, 600) 
+  createCanvas(300, 300) 
 }
 
 function draw() {
-  background(000);
+  background(100);
+  
 
-  if ((position > width) ||  position < 0) {
+  if ((position > (width-squareSize)) ||  position < 0) {
     amountToMovePerCycle = amountToMovePerCycle * -1;
+    count++;
   }
-    // amountToMovePerCycle = -0.5;
-  // } else if (position < 0) {
-    // amountToMovePerCycle = 0.5;
-  // }
+
   if (moving) {
     position = position + amountToMovePerCycle;
   }
-  rect(position,0,10,10);
+  rect(position,0,squareSize,squareSize);
+  text('The number of cycles is: ' + count, 10, 30);
 } 
 
 
@@ -28,3 +30,7 @@ function mousePressed() {
   moving = !moving;
 }
 
+    // amountToMovePerCycle = -0.5;
+  // } else if (position < 0) {
+    // amountToMovePerCycle = 0.5;
+  // }
