7808bf5b4acd4feacc03cbd4f8d1b13281cd2900
Rebecca Keegan <rebeccachristinekeegan@gmail.com>
2019-04-02 20:12:22 +0100

    Square moves back and forth

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -1,22 +1,30 @@
 let position = 100;
 let moving = true;
-
+let amountToMovePerCycle = 3;
 
 function setup() {
-  createCanvas(400, 200)
+  createCanvas(600, 600) 
 }
 
 function draw() {
-  if(moving){
-    background(100)
-    position = position + 0.5
-    rect(position,0,10,10)
+  background(000);
+
+  if ((position > width) ||  position < 0) {
+    amountToMovePerCycle = amountToMovePerCycle * -1;
   }
-  // console.log('The position is ' + position)
-  // console.log('The position is ' + moving)
-}
+    // amountToMovePerCycle = -0.5;
+  // } else if (position < 0) {
+    // amountToMovePerCycle = 0.5;
+  // }
+  if (moving) {
+    position = position + amountToMovePerCycle;
+  }
+  rect(position,0,10,10);
+} 
+
 
 function mousePressed() {
   console.log('mouse pressed')
   moving = !moving;
 }
+
