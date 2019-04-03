637913f731d0b85550b4bd87d0ab2264a30199f7
Rebecca Keegan <rebeccachristinekeegan@gmail.com>
2019-04-03 14:55:31 +0100

    Pong game created

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -1,54 +1,103 @@
-const squareSize = 10;
+//ball
+const diameter = 20;
 let positionX = 100;
 let positionY = 100;
 let moving = true;
-let count = 0;
+let bounceCount = 0;
+let leftCount = 0;
 let xVelocity = -2;
 let yVelocity = 1;
+
+//paddle
+let xPaddle;
+let yPaddle;
+let paddleWidth = 100;
+let paddleHeight = 25;
+let started = false;
+
 
 function setup() {
   createCanvas(300, 300); 
 }
 
 function draw() {
-  background(100);
+  background(0);
+  fill(255, 0, 255);
+  noStroke();
 
-  if (isAtLefthandSide() ||  isAtRightHandSide()) {
+  //making ball move
+  if (isAtLeftHandSide() ||  isAtRightHandSide()) {
     xVelocity *= -1;
-    count++;
+    bounceCount++;
   }
 
   if (isAtTop() ||  isAtBottom()) {
     yVelocity *= -1;
-    count++;
+    bounceCount++;
   }
 
   if (moving) {
     positionX = positionX + xVelocity;
     positionY = positionY + yVelocity;
   }
-  rect(positionX,positionY,squareSize,squareSize);
-  text('Bounce Count: ' + count, 30, 30);
+  ellipse(positionX,positionY,diameter,diameter);
+  text('Bounce Count: ' + bounceCount, 30, 30);
+  
+  //when ball hits paddle
+  if (isAtPaddleLeft() && (isAtPaddleRight())) {
+    xVelocity *= -1;
+    yVelocity *= -1;
+  }
+
+  fill(0, 255, 255);
+  noStroke;
+  rect (xPaddle, yPaddle, paddleWidth, paddleHeight)
+
+  if (!started) {
+    xPaddle = width/2;
+    yPaddle = height-100;
+    started = true;
+  }
 } 
+
+function isAtPaddleRight() {
+  return (positionY  + (diameter/2) >= yPaddle);
+}
+
+function isAtPaddleLeft() {
+  return (positionX > xPaddle && positionX < xPaddle + paddleWidth);
+}
 
 function mousePressed() {
   console.log('mouse pressed')
   moving = !moving;
 }
 
-function isAtLefthandSide() {
-  return (positionX > (width - squareSize));
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    xPaddle -= 50;
+  } else if (keyCode === RIGHT_ARROW) {
+    xPaddle += 50;
+  } else if (keyCode === UP_ARROW) {
+    yPaddle -= 50;
+  } else if (keyCode === DOWN_ARROW) {
+    yPaddle += 50;
+  }
 }
 
 function isAtRightHandSide() {
-  return positionX < 0;
+  return (positionX > (width - diameter/2));
+}
+
+function isAtLeftHandSide() {
+  return (positionX < (0 + diameter/2));
 }
 
 function isAtTop() {
-  return (positionY > (height - squareSize));
+  return (positionY > (height - diameter/2));
 }
 
 function isAtBottom() {
-  return positionY < 0;
+  return (positionY < (0 + diameter/2));
 }
 
